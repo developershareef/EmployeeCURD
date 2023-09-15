f371b3d888feb56e7854a393b196f0c3508faeda
developershareef <shareefpasha.shaik.dev.com>
2023-09-15 17:30:20 +0530

    first commit

---
+++ EmployeeDemoCURD/Controllers/EmpDetailsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDemoCURD;
+
+namespace EmployeeDemoCURD.Controllers
+{
+   
+    public class EmpDetailsController : Controller
+    {
+        private EmployeeCRUDEntities db = new EmployeeCRUDEntities();
+
+        // GET: EmpDetails
+        public ActionResult Index()
+        {
+            return View(db.EmpDetails.ToList());
+        }
+
+        // GET: EmpDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmpDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpId,EmpName,EmpAdress,PhoneNo,Salary")] EmpDetail empDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmpDetails.Add(empDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empDetail);
+        }
+
+        // POST: EmpDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpId,EmpName,EmpAdress,PhoneNo,Salary")] EmpDetail empDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empDetail);
+        }
+
+        // POST: EmpDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            db.EmpDetails.Remove(empDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
